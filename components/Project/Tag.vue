fa3817d0d754a75d57b473eb9d841f497d2cd1e1
aladac <adam.ladachowski@gmail.com>
2022-06-26 00:20:51 +0200

    wip

--- components/Project/Tag.vue
+++ components/Project/Tag.vue
@@ -16,13 +16,15 @@ export default {
 </script>
 <style>
 .tag {
+  font-size: 1.2rem;
+  background-color: white;
   margin-top: 0.5rem;
   margin-right: 0.5rem;
-  color: white;
+  color: red;
   border: 2px solid white;
   border-radius: 0.4rem;
-  padding: 0.2rem;
-  min-width: 8rem;
+  padding: 0.3rem;
+  min-width: 9rem;
   display: inline-block;
 }
 </style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <ProjectItem title="Roomzilla" from="2016-11-01" to="2021-08-01">
-      sdsdsdsdsdsds
+      A SaaS resource booking system. Originally specific to conference room,
+      eventually allowing for booking almost any office resource.
+      <ul>
+        <li>
+          Tablet web app for direct control and status display of a resource
+        </li>
+        <li>Rails 6 backend</li>
+        <li>Vue 2.6 frontend</li>
+      </ul>
       <br />
       <ProjectTag name="Ruby" icon="ruby-plain" />
       <ProjectTag name="PostgreSQL" icon="postgresql-plain" />
+      <ProjectTag name="Vue.js" icon="vuejs-plain" />
+      <ProjectTag name="Heroku" icon="heroku-plain" />
     </ProjectItem>
   </div>
 </template>
