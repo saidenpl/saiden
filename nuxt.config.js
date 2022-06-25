62d2e1ad9b057d9dbab053cb69a57664cc7fb6eb
aladac <adam.ladachowski@gmail.com>
2022-06-25 23:22:44 +0200

    devicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css' }
+
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
