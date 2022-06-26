0a27c5693113510cd74041ad080c5fc142e3d16b
aladac <adam.ladachowski@gmail.com>
2022-06-26 13:39:24 +0200

    wording

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar">
     <NuxtLink class="navbar-link" to="/">Welcome</NuxtLink>
-    <NuxtLink class="navbar-link" to="/about">About</NuxtLink>
+    <NuxtLink class="navbar-link" to="/about">About Me</NuxtLink>
     <NuxtLink class="navbar-link" to="/projects">Projects</NuxtLink>
     <!-- <NuxtLink class="navbar-link" to="/hobbies">Hobbies</NuxtLink> -->
     <NuxtLink class="navbar-link" to="/contact">Contact</NuxtLink>
