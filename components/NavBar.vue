0cb40e661885fa67a1286a6343583de5059402cb
aladac <adam.ladachowski@gmail.com>
2022-06-26 13:37:37 +0200

    Removing hobbies

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,7 +3,7 @@
     <NuxtLink class="navbar-link" to="/">Welcome</NuxtLink>
     <NuxtLink class="navbar-link" to="/about">About</NuxtLink>
     <NuxtLink class="navbar-link" to="/projects">Projects</NuxtLink>
-    <NuxtLink class="navbar-link" to="/hobbies">Hobbies</NuxtLink>
+    <!-- <NuxtLink class="navbar-link" to="/hobbies">Hobbies</NuxtLink> -->
     <NuxtLink class="navbar-link" to="/contact">Contact</NuxtLink>
   </div>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>Foobar</div>
+  <div>
+    <p>
+      <i class="contacts-icon devicon-github-original"></i
+      ><a target="_blank" href="https://github.com/aladac"
+        >https://github.com/aladac</a
+      >
+    </p>
+    <p>
+      <i class="contacts-icon devicon-linkedin-plain"></i
+      ><a target="_blank" href="https://linkedin.com/in/aladac/"
+        >https://linkedin.com/in/aladac</a
+      >
+    </p>
+    <p>
+      <i class="contacts-icon fa-solid fa-envelope"></i
+      ><a target="_blank" href="mailto:aladac@saiden.pl">aladac@saiden.pl</a>
+    </p>
+  </div>
 </template>
+
+<style>
+i.contacts-icon {
+  margin-right: 0.5rem;
+}
+</style>
